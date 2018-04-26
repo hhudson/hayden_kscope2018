0fd4d8abb164fb4d801530ec9332bd302356286f
Hayden Hudson <hayden.hudson@concept2completion.com>
2018-04-25 21:45:31 -0400

    added app

--- trunk/release/_release.sql
+++ trunk/release/_release.sql
@@ -1,5 +1,7 @@
+
 @trunk/packages/sample_pkg.pks;
-@trunk/build/create_tables.sql;
+--@trunk/build/create_tables.sql;
 @trunk/packages/sample_pkg.pkb;
 @trunk/packages/sample_data_pkg.pks;
 @trunk/packages/sample_data_pkg.pkb;
+@trunk/apex/f130.sql;
